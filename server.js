b54c25efef215c38e6b85b588eb38c88c3f60055
ProgrammingWithMuzamil <muhmmadmuzamil445@gmail.com>
2026-01-31 20:44:29 +0500

    add

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.use(helmet({
             styleSrc: ["'self'", "'unsafe-inline'"],
             scriptSrc: ["'self'"],
             imgSrc: ["'self'", "data:", "http:", "https:"],
-            connectSrc: ["'self'", "https://ilandpropertiesdashboard.vercel.app", "https://damac-backend.onrender.com"]
+            connectSrc: ["'self'", "https://admin.ilandproperties.ae", "https://damac-backend.onrender.com"]
         },
     },
     crossOriginEmbedderPolicy: false
